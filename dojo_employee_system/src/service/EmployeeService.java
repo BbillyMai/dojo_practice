44314b30c18d29bf4f7eb9106f403c3fdb10b673
Billy Mai <billy.mai@oocl.com>
2019-12-22 15:22:11 +0800

    feat: get employees sorted by name

--- dojo_employee_system/src/service/EmployeeService.java
+++ dojo_employee_system/src/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dao.EmployeeDao;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import model.Employee;
@@ -12,5 +13,12 @@ public class EmployeeService {
     List<Employee> allEmployee = employeeDao.getAllEmployee();
     List<Employee> adultEmployee = allEmployee.stream().filter(Employee::isAdult).collect(Collectors.toList());
     return adultEmployee;
+  }
+
+  public List<Employee> getEmployeesSortedByName() {
+    EmployeeDao employeeDao = new EmployeeDao();
+    List<Employee> allEmployee = employeeDao.getAllEmployee();
+    List<Employee> employeesSortedByName = allEmployee.stream().sorted(Comparator.comparing(Employee::getName)).collect(Collectors.toList());
+    return employeesSortedByName;
   }
 }
--- dojo_employee_system/test/service/EmployeeServiceTest.java
+++ dojo_employee_system/test/service/EmployeeServiceTest.java
@@ -2,8 +2,6 @@ package service;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import model.Employee;
 import org.junit.Test;
@@ -11,9 +9,8 @@ import org.junit.Test;
 public class EmployeeServiceTest {
 
   @Test
-  public void should_return_older_than_18_employees_invoke_getAdultEmployee() {
-    EmployeeService employeeService = new EmployeeService();
-    List<Employee> adultEmployees = employeeService.getAdultEmployee();
+  public void should_return_older_than_18_employees_when_invoke_getAdultEmployee() {
+    List<Employee> adultEmployees = getEmployeeService().getAdultEmployee();
 
     assertEquals(2, adultEmployees.size());
     assertEquals("Sepp", adultEmployees.get(0).getName());
@@ -21,4 +18,19 @@ public class EmployeeServiceTest {
     assertEquals("Mike", adultEmployees.get(1).getName());
     assertEquals(51, adultEmployees.get(1).getAge());
   }
+
+  @Test
+  public void should_return_employees_sorted_by_name_when_invoke_getEmployeesSortedByName() {
+    List<Employee> adultEmployees = getEmployeeService().getEmployeesSortedByName();
+
+    assertEquals("Max", adultEmployees.get(0).getName());
+    assertEquals("Mike", adultEmployees.get(1).getName());
+    assertEquals("Nina", adultEmployees.get(2).getName());
+    assertEquals("Sepp", adultEmployees.get(3).getName());
+  }
+
+  private EmployeeService getEmployeeService() {
+    return new EmployeeService();
+  }
+
 }
